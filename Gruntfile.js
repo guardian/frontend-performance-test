24e11412212281f617feb9a657d5f9bce2a5285a
Darren Hurley <darren.hurley@guardian.co.uk>
2013-10-08 15:41:20 +0100

    moving into gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+/* global module: false */
+module.exports = function (grunt) {
+
+    var wptArgs = [
+            ['-s', grunt.option('server')],
+            ['--breakdown'],
+            ['--reporter', 'spec'],
+            ['--poll'],
+            ['--first'],
+            ['-l', 'EU_WEST_wptdriver:Chrome']
+        ],
+        domains = {
+            code: 'm.code.dev-theguardian.com',
+            prod: 'www.theguardian.com'
+        },
+        urls = {
+            front: 'uk',
+            article: 'news/2013/oct/07/jfk-assassination-creepiest-detail-parkland'
+        };
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Run shell commands
+        shell: {
+            wpt: {
+                command: function (app) {
+                    // default to code
+                    var env = grunt.option('env') || 'code',
+                        domain = domains[env];
+                    grunt.log.oklns('Running against `' + domain + '`')
+                    return './node_modules/webpagetest/bin/webpagetest test http://' + domain + '/' + urls[app] + '?view=mobile ' + wptArgs.map(function(arg) {
+                            return arg.join(' ')
+                        }).join(' ') + ' --specs specs/' + app + '-' + env + '.json';
+                },
+                options: {
+                    stdout: true,
+                    stderr: true
+                }
+            }
+        }
+
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-shell');
+
+    // Standard tasks
+    grunt.registerTask('perf', function(app) {
+        if (!app) {
+            grunt.fail.warn('App required, e.g. `grunt perf:article`.');
+        }
+        if (!urls[app]) {
+            grunt.fail.warn('Unknown app `'+ app + '`. Available apps - `' + Object.keys(urls).join('`, `') + '`.');
+        }
+        grunt.task.run(['shell:wpt:' + app]);
+    });
+
+};
--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-frontend-performance-test
+Frontend Performance Test
 =========================
 
 Suite of client-side performance tests to run against the frontend project. These enable us to monitor and alert against key thresholds for client-side assets sizes.
 
-To run
+Setup
 ------
 
     $ npm install
-    $ ./node_modules/webpagetest/bin/webpagetest test http://www.theguardian.com/uk?view=mobile -s http://ec2-box.com/  --breakdown  --reporter=spec --poll --first -l EU_WEST_wptdriver:Chrome --specs specs/tests.json
 
+To run
+------
+
+e.g. to run the `front` performance tests
+
+    $ grunt perf:front --server wpt-box.com
+
+`article` also available.
